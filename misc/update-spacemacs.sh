8b0aacd81999b8fdcb9683aeaf16b7cba74c7ca3
Rafael Guerra Paz <mr.rafaelgp@gmail.com>
2015-11-28 20:01:34 +0000

    Run script from anywhere. Use init-functions to display progress

--- misc/update-spacemacs.sh
+++ misc/update-spacemacs.sh
@@ -1,7 +1,15 @@
-cd ~/.emacs.d
-echo "git pull --rebase"
-git pull --rebase origin master
-echo "git submodule sync"
-git submodule sync
-echo "git submodule update"
-git submodule update
+#!/usr/bin/env bash
+
+if [ ! -d /lib/lsb/init-functions ]; then
+    source /lib/lsb/init-functions
+fi
+
+log_action_begin_msg $"git pull --rebase..."
+git -C ~/.emacs.d/ pull --quiet --rebase origin master
+log_action_end_msg $?
+log_action_begin_msg "git submodule sync"
+git -C ~/.emacs.d/ submodule sync
+log_action_end_msg $?
+log_action_begin_msg "git submodule update"
+git -C ~/.emacs.d/ submodule update
+log_action_end_msg $?
