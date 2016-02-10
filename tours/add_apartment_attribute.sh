5513208bbdbbcf7ca1d66733f2bb1e8909c8910c
Rafael Guerra Paz <mr.rafaelgp@gmail.com>
2016-02-10 12:03:05 +0000

    add_apartment_attribute -> Change path to load other apartment in the same frame

--- tours/add_apartment_attribute.sh
+++ tours/add_apartment_attribute.sh
@@ -4,10 +4,14 @@
 # Script made for Clarendon apartments
 # It will search for scene*.xml files and:
 # 1- Delete the first line
-# 2- Add a new <scene /> line with the scene name and the corresponding apartment 
-
+# 2- Add a new <scene /> line with the scene name and the corresponding apartment
+# Then it will change the path from:
+# %SWFPATH%/scenes
+# to:
+# %SWFPATH%/../../ApartmentFolderName/files/scenes
+# This change ensures that the "more_apartments" button loads the corresponding tour inside the same frame
 # USAGE:
-# Run from directory g:/virtual_tours/clarendon_apartments/
+# Run from directory e:/virtual_tours/clarendon_apartments/
 
 for eachdirectory in $(find ./* -maxdepth 0 -type d ); do
     # echo $eachdirectory
@@ -20,10 +24,13 @@ for eachdirectory in $(find ./* -maxdepth 0 -type d ); do
         each_xml_file=$(basename $each_xml_file_path)
         extension="${each_xml_file##*.}"
         each_xml_file="${each_xml_file%.*}"
+        each_xml_file_path_bck=$each_xml_file_path"_bck"
 
         first_line='<scene name="'$each_xml_file'" apartment="'$eachdirectory'">'
 
         sed -i -e "1c $first_line" $each_xml_file_path
+        sed -e 's/\%SWFPATH\%\/scenes/\%SWFPATH\%\/..\/..\/'$eachdirectory'\/files\/scenes/g' $each_xml_file_path > $each_xml_file_path_bck
+        mv $each_xml_file_path_bck $each_xml_file_path
 
         echo "Edited $each_xml_file"
 
