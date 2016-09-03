8f932c0cd2274cb7cd1fff20c5b4c1a3ba0219fb
Rafael Guerra Paz <mr.rafaelgp@gmail.com>
2016-09-03 08:15:42 +0100

    check-krpano.py > First commit

---
+++ misc/check-krpano.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from lxml import html
+import requests
+import time
+import tweepy
+# Import tweeter private keys
+from private import cfg
+
+def get_api(cfg):
+    auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+    auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
+    return tweepy.API(auth)
+
+def main():
+    page = requests.get('http://krpano.com/download/')
+    tree = html.fromstring(page.content)
+
+    version = tree.xpath('//div[@class="paragraph"]//h2/text()')
+    buildDate = tree.xpath('//span[@class="smallcomment"]/text()')
+
+    # Comver list to string
+    version = ''.join(version)
+    buildDate = ''.join(buildDate)
+
+    # Remove 7 first characters "(build "
+    BuildDateClean = buildDate[7:]
+    # Remove last character ")"
+    BuildDateClean = BuildDateClean[:-1]
+
+    today = time.strftime("%Y-%m-%d")
+    # today = BuildDateClean
+
+    # print(version);
+    # print(BuildDateClean);
+    # print(today)
+
+    if BuildDateClean == today :
+        api = get_api(cfg)
+        tweet = version + buildDate + ' released! Click on the link underneath to download #krpano http://krpano.com/download/'
+        status = api.update_status(status=tweet)
+
+
+    print('_check-kpano-EOF_')
+
+if __name__ == "__main__":
+    main()
