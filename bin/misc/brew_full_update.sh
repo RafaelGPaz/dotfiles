87cae51c020661d81ed2331016f5798e85c806a2
Rafael G Paz <rafael@tourvista.co.uk>
2018-02-15 16:47:42 +0000

    Added script brew_full_update.sh

---
+++ bin/misc/brew_full_update.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+    brew update
+
+    if [ "$1" != '--quick' ]; then
+      printf "Removing brew cache\n"
+      brew cleanup --force -s && rm -rf $(brew --cache)
+      printf "Running brew update\n"
+      brew update
+    fi
+    for c in $(brew cask list); do
+
+        current="$(brew cask info $c | sed -n '1p' | sed -n 's/^.*: \(.*\)$/\1/p')"
+        installed=( $(ls /usr/local/Caskroom/$c))
+
+      if (! [[ " ${installed[@]} " == *" $current "* ]]); then
+            printf "[ INFO ] $c (${installed[@]}) -----> $current\n"
+            select ynx in "Yes" "No" "Exit"; do
+              case $ynx in
+                "Yes") echo "Uninstalling $c"; brew cask uninstall --force "$c"; echo "Re-installing $c"; brew cask install "$c"; break;;
+                "No") echo "Skipping $c"; break;;
+                "Exit") echo "Exiting brew-cask-upgrade"; return;;
+              esac
+            done
+        else
+            printf "[  OK  ] $c ($installed)\n"
+        fi
+    done
+
+    brew upgrade
