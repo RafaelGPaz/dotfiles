9adb52d655f46ccbafad7b6be25cf8583ebeb9d0
Rafael G Paz <rafael@tourvista.co.uk>
2019-05-23 15:36:33 +0100

    aldi-spanish-week.py > Fix

--- bin/misc/aldi-spanish-week.py
+++ bin/misc/aldi-spanish-week.py
@@ -1,23 +1,21 @@
-#! python3
+#!/usr/bin/env python
+
 import bs4, requests, smtplib
 
-toAddress = 'mr.rafaelgp@gmail.com'
-
-#Download page
 getPage = requests.get('https://www.lidl.co.uk/en/Offers.htm?week=1')
 getPage.raise_for_status() #if error it will stop the program
 
 content = bs4.BeautifulSoup(getPage.text, 'html.parser')
 images = content.findAll('img')
-spain = 'slot_image_spain.png' # This is the name of the food you are looking for
+spain = 'slot_image_spain.png'
 available = False
 
 for image in images:
     if image['alt'] == 'Flavour of the Week':
-        if image['alt'].find(spain) == -1:
+        if image['src'].find(spain) != -1:
             available = True
 
 if available == True:
-    print('Spanish week in the Aldi')
+    print('Spanish week in Aldi :-)')
 else:
-    print('No Spanish week')
+    print('No Spanish week :-(')
