33410aed8414b185c43d16b564aa6092b321309f
Rafael G Paz <rafael@tourvista.co.uk>
2020-12-04 19:26:33 +0000

    Use brew --cask

--- bin/misc/brew_full_update.sh
+++ bin/misc/brew_full_update.sh
@@ -8,18 +8,18 @@
       printf "Running brew update\n"
       brew update
     fi
-    for c in $(brew cask list); do
+    for c in $(brew list --cask); do
         if [ "$c" == "carbon-copy-cloner" ]; then
           continue;
         fi
-        current="$(brew cask info $c | sed -n '1p' | sed -n 's/^.*: //p' | sed -e 's/ (auto_updates)//g')"
+        current="$(brew info --cask $c | sed -n '1p' | sed -n 's/^.*: //p' | sed -e 's/ (auto_updates)//g')"
         installed=( $(ls /usr/local/Caskroom/$c))
 
       if (! [[ " ${installed[@]} " == *" $current "* ]]); then
             printf "[ INFO ] $c (${installed[@]}) -----> $current\n"
             select ynx in "Yes" "No" "Exit"; do
               case $ynx in
-                "Yes") echo "Uninstalling $c"; brew cask uninstall --force "$c"; echo "Re-installing $c"; brew cask install "$c"; break;;
+                "Yes") echo "Uninstalling $c"; brew uninstall --cask --force "$c"; echo "Re-installing $c"; brew install --cask "$c"; break;;
                 "No") echo "Skipping $c"; break;;
                 "Exit") echo "Exiting brew-cask-upgrade"; return;;
               esac
